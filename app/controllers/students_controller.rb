fcd6b53e7822d819aaed6b802732cc28f7b7f61b
as30098 <alexspencer421@gmail.com>
2020-12-14 22:19:35 -0500

    commit tree dabfe9e22bec89e84a57335d9f9dade2e6a864d2

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,34 @@
+class StudentsController < ApplicationController
+
+  def index
+    @students = Student.all
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new
+    @student.first_name = params[:student][:first_name]
+    @student.last_name = params[:student][:last_name]
+    @student.save
+    redirect_to student_path(@student)
+  end
+
+  #edit
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  #update
+  def update
+    @student = Student.find(params[:id])
+    @student.update(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    redirect_to student_path(@student)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #new
+  #create
+  #show
+  #edit
+  #update
+
+  resources :students, except: [:destroy, :index]
+  resources :school_classes, except: [:destroy, :index]
+
 end
